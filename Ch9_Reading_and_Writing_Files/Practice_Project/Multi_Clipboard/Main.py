b1cdabf32ddd37bba4391811bde611b8edad9ac9
Xiongkai <conan7153@gmail.com>
2023-04-10 10:11:15 +0800

    Extend feature for Multi-Clipboard Practice Project CAA 100423 1010 hrs

--- Ch9_Reading_and_Writing_Files/Practice_Project/Multi_Clipboard/Main.py
+++ Ch9_Reading_and_Writing_Files/Practice_Project/Multi_Clipboard/Main.py
@@ -5,6 +5,8 @@ Project requirements:
 to have the current message in the clipboard saved in the table in the form of {keyword:message}
 3, Users can access the list of available keywords in the system by typing Main.py list, the list of keywords
 would be in alphabetical orders.
+4, Users can delete the keyword in the table using Main.py delete [keyword] command, or clear the table of content
+using Main.py clear command
 '''
 import pyperclip as pyclip
 import sys, shelve
@@ -14,9 +16,20 @@ keywordShelf = shelve.open("mcbkeyword")
 
 #Save and update shelve value 
 if len(sys.argv) == 3:
-    filename, save, keyword = sys.argv
-    keywordShelf[keyword] = pyclip.paste()
-    print(f"Message for {keyword} successfully saved.")
+    filename, action, keyword = sys.argv
+    if action == "save":
+        keywordShelf[keyword] = pyclip.paste()
+        print(f"Message for {keyword} successfully saved.")
+    if action == "delete":
+        if keyword.lower() in keywordShelf.keys():
+            userInput = input(f"You are deleting message for keyword {keyword}. Do you want to proceed?\nEnter 'Y' to proceed, enter anything else to close program: ")
+            if userInput == "Y":
+                keywordShelf.pop(keyword)
+                print(f"Keyword {keyword} has been deleted from the table.")
+            else:
+                sys.exit()
+        else:
+            print(f"Keyword {keyword} not found in the table.")
 
 #Copy value from shelve and paste to clipboard
 elif len(sys.argv) == 2:
@@ -31,10 +44,21 @@ elif len(sys.argv) == 2:
         keywordList.sort(key = str.lower)
         for key in keywordList:
             print(key)
-
+    
+    #Clearing keyword table
+    elif keyword.lower() == "clear":
+        userInput = input("You are erasing the table of keywords using clear command, do you want to proceed?\nEnter 'Y' to proceed, enter anything else to close program: ")
+        if userInput == "Y":
+            keywordShelf.clear()
+            print("Keyword table has been cleared.")
+        else:
+            sys.exit()
+            
     #Keyword not found error message
     else:
-        print(f"No Message found for keyword {keyword}\nYou can use Main.py save [keyword] to save message into the system.")
+        print(f"Keyword {keyword} not found.\nYou can use Main.py save [keyword] to save message into the system.")
+
+    
 
 #Close the shelve when the program terminates
 keywordShelf.close()
